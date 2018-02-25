29eebbbbc11889ab8fdd9c7b1510225744c5202a
Paul Pieper <paulmpieper@gmail.com>
2018-02-25 00:16:20 -0500

    -fixed a race condition between OnGUI and Update/LateUpdate -fixed a nullref caused by using Camera.current-use 'main' instead -slightly optimized code

--- Assets/Scripts/ScanResults.cs
+++ Assets/Scripts/ScanResults.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ScanResults : MonoBehaviour {
 	
 	private GameObject scannableObject;
-	private Vector3 objectLocation;
+	private Vector3 objectScreenPos;
 	private GameObject playerObject;
 	private Vector3 playerPos;
 	private int distance;
@@ -24,29 +24,36 @@ public class ScanResults : MonoBehaviour {
 	// initializations
 	private void Start() 
 	{
-		scannableObject = this.gameObject;
-		objectLocation = new Vector3();
+		scannableObject = gameObject;
+		objectScreenPos = scannableObject.transform.position;
 		playerObject = GameObject.Find("FPSChar1");
 		playerPos = playerObject.transform.position;
 	}    
 
-	// display the results if scanner is on (called multiple times per frame)
+	// called multiple times per frame
+	// display the results if scanner is on and object is in front of camera
 	private void OnGUI()
 	{
 		if (scanActive) 
 		{
-			GUI.skin.font = scanFont;
-			GUI.contentColor = color;
-			GUI.Label (new Rect (objectLocation.x, Screen.height - objectLocation.y, 500, 30), scanResult);
+			// if the camera is facing the object, the z axis will be non-negative
+			if (objectScreenPos.z >= 0) {
+				GUI.skin.font = scanFont;
+				GUI.contentColor = color;
+				GUI.Label (new Rect (objectScreenPos.x, Screen.height - objectScreenPos.y, 500, 30), scanResult);
+			}
 		}
 	}    
 
-	// called once per frame, check to see if the player is currently scanning
+	// called once per frame
+	// check to see if the player is currently scanning
 	void Update()
 	{
 		// check to see if the Scan key is held down 
 		if (Input.GetButton("Scan"))
 		{
+			// always ensure that the location is up to date before the GUI is displayed
+			objectScreenPos = Camera.main.WorldToScreenPoint(scannableObject.transform.position);
 			scanActive = true;
 		} else 
 		{
@@ -54,24 +61,22 @@ public class ScanResults : MonoBehaviour {
 		}
 	}
 
-	// called after Update()
+	// called after Update(), once per frame
 	// Use LateUpdate to compute the distance after the player or object has moved, and add it to the results
 	private void LateUpdate()
 	{
-		// save some resources by only doing the computations while user is scanning
+		// save some resources by only doing the distance computations/string concatenation while user is scanning
 		if (!scanActive) 
 		{
 			return;
 		}
 
-		// if the camera is facing away from the object, the z axis will be negative
-		if (Camera.current.WorldToScreenPoint(scannableObject.transform.position).z >= 0)
+		// if the camera is facing the object, the z axis will be non-negative
+		if (objectScreenPos.z >= 0)
 		{
-			objectLocation = Camera.current.WorldToScreenPoint(scannableObject.transform.position);
 			playerPos = playerObject.transform.position;
 			distance = (int)Vector3.Distance(scannableObject.transform.position, playerObject.transform.position);
 			scanResult = scanText + " - " + distance.ToString() + "km";
-
 		}
 	}    
 }

a142d47c2a78ba1e4a80a318916f6a050cc814eb
Paul Pieper <paulmpieper@gmail.com>
2018-03-06 19:48:05 -0500

    - added two enemy ship assets and other misc. assets - other assets: a crate, a cargo container, and a satellite - added default font/color values to ScanResults for convenience

--- Assets/Scripts/ScanResults.cs
+++ Assets/Scripts/ScanResults.cs
@@ -14,10 +14,19 @@ public class ScanResults : MonoBehaviour {
 	[TextArea(0,1)]
 	// Input text to display on player's scanner
 	public string scanText;
-	// Input font to use for the text display
+	// Input font to use for the text display 
 	public Font scanFont;
 	// Input color of the text displayed
 	public Color color;
+
+	// called in editor when reset button is pressed, or when script is first attached
+	private void Reset() 
+	{
+		// set the default inspector values for convenience
+		scanFont = Resources.Load("Fonts/nk57-monospace-cd-bd") as Font;
+		// rgba 195 250 255 255
+		color = new Color(.765F,.98F, 1F, 1F);
+	}    
 
 	// initializations
 	private void Start() 

6a1cbe7edd741a17243b8337b5b43fd8862529ee
Dom <dominick.good@gmail.com>
2018-03-08 20:29:52 -0500

    ammo, currency, fixed FDT, test push

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,9 +1,11 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Gun : MonoBehaviour {
+public class Gun : MonoBehaviour
+{
 
-    
+
     public float range = 100f;
     public System.Random rnd = new System.Random();
     public int damageMin = 150;
@@ -12,47 +14,84 @@ public class Gun : MonoBehaviour {
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
     public GameObject FDT;  //temp code
-	// Update is called once per frame
-	void Update () {
-		
-        if(Input.GetButtonDown("Fire1"))
+    public int totalAmmo = 20;
+    public int totalCurrency = 0;
+    public Text ammoText;
+    public Text currencyText;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
 
         }
+        ammoText.text = "Ammo: " + totalAmmo.ToString();
+        currencyText.text = "Currency: " + totalCurrency.ToString();
+    }
 
-	}
 
-
-    void Shoot ()
+    void Shoot()
     {
-        muzzleFlash.Play();
-        RaycastHit hit;
-       if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range) )
+        if (totalAmmo > 0)
         {
-            Debug.Log(hit.transform.name);
+            RaycastHit hit;
+            muzzleFlash.Play();
+            totalAmmo = (totalAmmo - 1);
 
-            Target target = hit.transform.GetComponent<Target>();
-            if (target != null) //if the shot hits
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
             {
 
-                
-    int dmg = rnd.Next(damageMin, damageMax);  //dmg range
-                target.TakeDamage(dmg);
-                Debug.LogFormat("{0} was dealt {1} damage", target, dmg);
-                TextMesh textObject = GameObject.Find("FDT").GetComponent<TextMesh>();
-                textObject.text = dmg.ToString();
-                GameObject FDTgo = Instantiate(FDT, hit.point, Quaternion.identity); //temp code
-                
-                textObject.text = " ";
-                Destroy(FDTgo, 0.15f);  //temp code
-                
-                
-                //above is creating and destroying the combat text
-            }
 
-            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactGO, 1f);
+
+
+
+                //muzzleFlash.Play();
+                Debug.Log(hit.transform.name);
+
+                Target target = hit.transform.GetComponent<Target>();
+
+
+                //
+
+                if (target != null) //if the shot hits
+                {
+
+                    //AmmoIncreaseDrop();
+                    int dmg = rnd.Next(damageMin, damageMax);  //dmg range
+                    target.TakeDamage(dmg);
+                    Debug.LogFormat("{0} was dealt {1} damage", target, dmg);
+                    TextMesh textObject = GameObject.Find("FDT").GetComponent<TextMesh>();
+                    textObject.text = dmg.ToString();
+                    GameObject FDTgo = Instantiate(FDT, hit.point, Quaternion.identity); //temp code
+
+                    textObject.text = " ";
+
+
+
+                    Destroy(FDTgo, 0.15f);//temp code
+
+
+                    //above is creating and destroying the combat text
+                }
+
+                GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGO, 1f);
+                if (target.health < 0)
+                {
+                    AmmoIncreaseDrop();
+                    totalCurrency = (totalCurrency + 10);
+                }
+
+
+            }
         }
+    }
+
+    public void AmmoIncreaseDrop()
+    {
+        totalAmmo = (totalAmmo + 6);
     }
 }

0e7cffb94c007a4ae1bafa89f9a81a7f1d7e834a
Endy Iskandar Imam <imam.endy@gmail.com>
2018-03-21 20:25:09 -0400

    Started working on UI

---
+++ Assets/Scenes/TestingScenes/GenerateCrosshair.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateCrosshair : MonoBehaviour {
+  public int CHDensity = 1;
+  public int CHCenterSize = 3;
+  public int CHLength = 5;
+  public int CHDistance = 5;
+
+  public GameObject basis;
+
+  private RectTransform rt;
+
+
+  void Awake ()
+  {
+    // set center
+  }
+
+	// Use this for initialization
+	void Start ()
+  {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+		
+	}
+}
---
+++ Assets/Scenes/TestingScenes/UIDebugTextOutput.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDebugTextOutput : MonoBehaviour {
+  public Camera cp;
+  public GameObject refer;
+
+  private GenerateCrosshair gen;
+
+  private Text t;
+
+  void Awake ()
+  {
+    t = GetComponent<Text>();
+    gen = refer ? refer.GetComponent<GenerateCrosshair>() : null;
+
+    set();
+  }
+
+  void Start ()
+  {
+    
+  }
+
+  void Update ()
+  {
+    set();
+  }
+
+
+  void set ()
+  {
+    string fov = cp ? cp.fieldOfView.ToString() : "N/A";
+    string den = gen ? gen.CHDensity.ToString() : "N/A";
+    string cen = gen ? gen.CHCenterSize.ToString() : "N/A";
+    string len = gen ? gen.CHLength.ToString() : "N/A";
+    string dis = gen ? gen.CHDistance.ToString() : "N/A";
+
+    t.text = "        Field of View: " + fov + " deg\n"
+           + "    Crosshair Density: " + den + " px\n"
+           + "Crosshair Center Size: " + cen + " px\n"
+           + "     Crosshair Length: " + len + " px\n"
+           + "   Crosshair Distance: " + dis + " px\n";
+  }
+}

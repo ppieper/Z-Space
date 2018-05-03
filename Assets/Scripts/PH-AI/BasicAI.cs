ef0cf88eab747234d391736ebdfb4d7b28d5cb88
Endy Iskandar Imam <endy.imam001@umb.edu>
2018-05-02 23:41:34 -0400

    Basic implementation of sensor and rework variables

--- Assets/Scripts/PH-AI/BasicAI.cs
+++ Assets/Scripts/PH-AI/BasicAI.cs
@@ -7,46 +7,55 @@ public class BasicAI : MonoBehaviour {
   // list of states
   enum States { CRUISE, APPROACH, ATTACK, DIVERT, OVERTAKE, REALIGN };
     // CRUISE :: Default state that just roam around until it senses a player
-    // APPROACH :: Approaches to the player target until it is on their crosshair
+    // APPROACH :: Approaches to the player target until it is on their cross-hair
     // ATTACK :: Fires at the player at the target
     // DIVERT :: When dangerously close, the enemy turns away to prevent crashing
     // OVERTAKE :: When closing in to the player, chasing, overtake onto the side
     // REALIGN :: After overtaking, turn around to reapproach to the player
 
-  // enemy count
-  public static int enemyCount = 0;
-
   // AI Properties
-  public float cruiseSpeed = 10.0f;    // speed of the AI when not in combat
-  public float fov = 60.0f;            // angle of view cone of AI to notice the player
-  public float viewRange = 2500.0f;    // effective range of the view cone of the AI to notice the player
-  public float senseRange = 500.0f;    // effective range of the sense sphere of the AI to notice the player
+  public float cruiseSpeed = 10.0f;       // speed of the AI when not in combat
+  // public float fov = 60.0f;               // angle of view cone of AI to notice the player
+  // public float viewRange = 2500.0f;       // effective range of the view cone of the AI to notice the player
+  public float senseRange = 2500.0f;      // effective range of the sense sphere of the AI to notice the player
 
-  public float combatSpeed = 75.0f;    // speed of the AI when in combat
-  public float turnSpeed = 200.0f;     // turn speed of the AI
+  public float combatSpeed = 75.0f;       // speed of the AI when in combat
+  public float turnSpeed = 200.0f;        // turn speed of the AI
 
-  public float overtakeRange = 25.0f;  // effective range of when they start to attempt to overttake you
-  public float sideOTRange = 5.0f;     // potential range of the distance of the player by the side
-  public float realignRange = 100.0f;  // effective range of when they move away from the player before returning back in
+  public float overtakeRange = 25.0f;     // effective range of when they start to attempt to overtake you
+  public float sideOvertakeRange = 5.0f;     // potential range of the distance of the player by the side
+  public float realignRange = 100.0f;     // effective range of when they move away from the player before returning back in
 
-  private Rigidbody rb;                // RigidBody of the AI
-  private Ray detect;                  // Ray for detecting stuff
+  private States state;                   // Current State of the AI
+  private States prev;                    // Previous State of the AI (for Divert)
 
-  private States state;                // Current State of the AI
-  private States prev;                 // Previous State of the AI (for Divert)
+  private Rigidbody enemyOwnRB;           // Rigidbody of the AI
+  private GameObject playerObject;        // Reference to the player ship for position and other what not
+
+  // private Ray detect;                     // Ray for detecting stuff
+  private Plane relativePlayerPlane;      // Plane for overtaking player
+  private float distanceToPlayerPlane;    // Distance for trying to overtake player
+  private float planarDistanceToPlayer;   // Distance on plane from player for successful overtake
+
 
   // Use this for initialization
-  void Start () {
-    enemyCount++;
-    rb = GetComponent<Rigidbody>();
-    rb.velocity = transform.forward * cruiseSpeed;
+  void Start ()
+  {
+    // get the player object for reference
+    playerObject = GameObject.Find("PH-Ship");
 
+    // get the enemy ship stuff for movement
+    rb = GetComponent<Rigidbody>();
+
+    // initialize the states
     prev = state = States.CRUISE;
-    RayUpdate();
+
+    //UpdateStuff();
   }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+  {
     if (state == States.CRUISE)
     {
       // ACTION :: Default state that just roam around until it senses a player
@@ -61,7 +70,7 @@ public class BasicAI : MonoBehaviour {
       else if (false)
       {
         state = States.APPROACH;
-        //  TRANSITION to ATTACK :: Check if they already have player in their crosshair
+        //  TRANSITION to ATTACK :: Check if they already have player in their cross-hair
         if (false) {
           state = States.ATTACK;
         }
@@ -78,7 +87,7 @@ public class BasicAI : MonoBehaviour {
       {
         prev = state;
         state = States.DIVERT;
-      } // TRANSITION to ATTACK :: Check if they have player in their crosshair
+      } // TRANSITION to ATTACK :: Check if they have player in their cross-hair
       else if (false)
       {
         state = States.ATTACK;
@@ -94,7 +103,7 @@ public class BasicAI : MonoBehaviour {
       {
         prev = state;
         state = States.DIVERT;
-      } // TRANSITION to APPROACH :: Check if they have player is out of their crosshair
+      } // TRANSITION to APPROACH :: Check if they have player is out of their cross-hair
       else if (false)
       {
         state = States.APPROACH;
@@ -185,17 +194,24 @@ public class BasicAI : MonoBehaviour {
       
     }
 
-    RayUpdate();
+    //UpdateStuff();
   }
 
 
-  private void RayUpdate()
+  private void UpdateStuff()
   {
-    detect.direction = transform.forward;
-    detect.origin = transform.position;
+    // set the relative player plane at the player with the normal of the enemy's forward
+    relativePlayerPlane = Plane(this.transform.forward, playerObject.transform.position);
+    // set the point on plane for the proceeding 
+    Vector3 pointOnPlane = relativePlayerPlane.ClosestPointOnPlane(this.transform.position);
+    // calculate the current distance to plane
+    distanceToPlayerPlane = Vector3.Distance(this.transform.position, pointOnPlane);
+    // calculate the current distance on plane to player
+    planarDistanceToPlayer = Vector3.Distance(pointOnPlane, playerObject.transform.position);
   }
 
   // debug on draw gizmo
+  /*
   private void OnDrawGizmos()
   {
     Gizmos.color = Color.red;
@@ -203,7 +219,7 @@ public class BasicAI : MonoBehaviour {
     Gizmos.DrawWireSphere(transform.position, senseRange);
     // draw view range
     Gizmos.DrawRay(transform.position, transform.forward * viewRange);
-    // draw overrtake range
+    // draw overtake range
     Gizmos.color = Color.green;
     Gizmos.DrawWireSphere(transform.position, overtakeRange);
     Gizmos.DrawWireSphere(transform.position, sideOTRange);
@@ -211,4 +227,5 @@ public class BasicAI : MonoBehaviour {
     Gizmos.color = Color.cyan;
     Gizmos.DrawWireSphere(transform.position, realignRange);
   }
+  */
 }

9f5053bd6d391d35f81eb9fa21c2040f9f4d36e2
Paul Pieper <paulmpieper@gmail.com>
2018-05-04 00:29:22 -0400

    Added explosion sound effect on killing an enemy -fixed explosion animation not always spawning in the correct location

--- Assets/Scripts/Enemy/Target.cs
+++ Assets/Scripts/Enemy/Target.cs
@@ -6,6 +6,7 @@ public class Target : MonoBehaviour {
     public float health = 50f;
 	public float shield = 5f;
 	public GameObject deathExplosion;
+	private bool dead = false;
 
 	void Start()
 	{
@@ -30,21 +31,40 @@ public class Target : MonoBehaviour {
 		}
         health -= amount;
 
-        if (health <= 0f)
+        if (health <= 0f && !dead)
         {
+			dead = true;
             Die();
         }
     }
 
     void Die ()
     {
+		// display an explosion and play the sound effect
 		if (deathExplosion) {
-			GameObject explosion = Instantiate (deathExplosion);
+			GameObject explosion = Instantiate(deathExplosion, gameObject.transform.position, gameObject.transform.rotation);
 			explosion.transform.SetParent(GameObject.FindGameObjectWithTag("WorldDynamic").transform);
 			Destroy (explosion, 1f);
+			AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+			audioSource.clip = Resources.Load("Sound/explosion") as AudioClip;
+			audioSource.Play();
+
 		}
-		EnemyManager.Instance.RemoveEnemy(gameObject.GetComponent<Enemy>());
-        Destroy(gameObject);
+		// remove enemy from enemy manager
+		Enemy enemy = gameObject.GetComponent<Enemy>();
+		EnemyManager.Instance.RemoveEnemy(enemy);
+
+        // destroy the indicators so they don't show up
+		Destroy(enemy);
+		// disable all the associated meshrenderers while we wait for object to be GC'd
+		MeshRenderer[] renderers = gameObject.GetComponentsInChildren<MeshRenderer>();
+		foreach(MeshRenderer renderer in renderers)
+		{
+			renderer.enabled = false;
+		}
+		// disable collisider
+		gameObject.GetComponent<BoxCollider>().enabled = false;
+		Destroy(gameObject, 2f);
 
     }
 }

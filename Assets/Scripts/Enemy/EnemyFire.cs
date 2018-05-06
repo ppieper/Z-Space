469df5d33bc1e6fdf44c612bdfdc6f1251bc9f94
Paul Pieper <paulmpieper@gmail.com>
2018-05-06 15:20:15 -0400

    Revert "level 1 rough draft + enemyfire script first draft with projectile placeholder prefab"
    This reverts commit cf82c0a60e49b6455589cdd309c0cf0e1e7af4a9.

--- Assets/Scripts/Enemy/EnemyFire.cs
+++ Assets/Scripts/Enemy/EnemyFire.cs
@@ -14,7 +14,8 @@ public class EnemyFire : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        
+        GameObject firebullet = Instantiate(projectile, transform.position, Quaternion.identity);
+        firebullet.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
         
         
     }
@@ -32,8 +33,7 @@ public class EnemyFire : MonoBehaviour {
 
     void Shoot()
     {
-
-        GameObject firebullet = Instantiate(projectile, spawn.transform.position, Quaternion.identity) as GameObject;
-        firebullet.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+        
+        transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
